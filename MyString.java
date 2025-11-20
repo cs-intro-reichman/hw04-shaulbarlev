d0ae4a07bd8df2c1ff604b4bdda120d530e7ace5
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-11-20 20:46:54 +0200

    itsaworking

--- MyString.java
+++ MyString.java
@@ -1,10 +1,10 @@
 public class MyString {
     public static void main(String[] args) {
-        System.out.println("Testing lowercase:");
-        System.out.println("UnHappy : " + lowerCase("UnHappy"));
-        System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
-        System.out.println("TLV : " + lowerCase("TLV"));
-        System.out.println("lowercase : " + lowerCase("lowercase"));
+        // System.out.println("Testing lowercase:");
+        // System.out.println("UnHappy : " + lowerCase("UnHappy"));
+        // System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
+        // System.out.println("TLV : " + lowerCase("TLV"));
+        // // System.out.println("lowercase : " + lowerCase("lowercase"));
 
         System.out.println("Testing contains:");
         System.out.println(contains("unhappy", "happy")); // true
@@ -18,13 +18,30 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String out = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 67 && c <= 90) {
+                c += 32;
+            }
+            out += c;
+        }
+        return out;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) == str2.charAt(0)) {
+                int hits = 0;
+                for (int j = 0; j < str2.length(); j++) {
+                    if (str1.charAt(i+j) == str2.charAt(j)) {
+                        hits++;
+                    }
+                }
+                if (hits == str2.length()) return true;
+            }
+        }
         return false;
     }
 }
