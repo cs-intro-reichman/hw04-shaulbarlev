452b57e06620da7aab70906a1bece262e14971c4
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-11-20 22:00:03 +0200

    kkee

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,14 +21,17 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
+        boolean[] flagged = new boolean[sentences.length];
 
         for (int s = 0; s < sentences.length; s++) {
             String sentence = MyString.lowerCase(sentences[s]);
             for (int k = 0; k < keywords.length; k++) {
                 if (MyString.contains(sentence, keywords[k])) {
-                    System.out.println(sentence);
+                    flagged[s] = true;
+                    // System.out.println(sentence);
                 }
             }
+            if (flagged[s]) System.out.println(sentences[s]);
         }
     }
 }
